6269caf445b4794c52ad08f8f42d2b7aad29af05
The Zuraiz <thezuraiz@gmail.com>
2025-10-20 20:47:55 +0500

    Add title and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import joblib
 st.set_page_config(page_title="Customer Churn Predictor", layout="centered")
 st.title("Customer Churn Predictor")
 
-st.write("Customer Churn Predictor")
+
 
 # Load artifacts
 model = joblib.load('best_churn_model.pkl')
